f6764068ebdc1c203ecd22c0c963bb07fa095969
chrisnabayuaji <chrisnabayuaji@gmail.com>
2018-07-05 13:18:09 +0700

    Change field

--- application/modules/par_client/views/form.php
+++ application/modules/par_client/views/form.php
@@ -7,33 +7,107 @@
     <form id="form" class="" action="<?=base_url()?>par_client/<?=$action?>" method="post">
       <div class="col-md-6">
         <input class="form-control keyboard" type="hidden" name="client_id" value="<?php if($client != null){echo $client->client_id;}?>">
+        <h4>Profil Perusahaan</h4>
         <div class="form-group">
-          <label>Nama Perusahaan <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_name" value="<?php if($client != null){echo $client->client_name;}?>">
+          <label>Nama Usaha <small class="required-field">*</small></label>
+          <input class="form-control" type="text" name="client_name" value="<?php if($client != null){echo $client->client_name;}?>">
         </div>
+        <div class="form-group">
+          <label>Nama Brand <small class="required-field">*</small></label>
+          <input class="form-control" type="text" name="client_brand" value="<?php if($client != null){echo $client->client_brand;}?>">
+        </div>
+        <div class="form-group">
+          <label>Status Usaha <small class="required-field">*</small></label>
+          <input class="form-control" type="text" name="client_status" value="<?php if($client != null){echo $client->client_status;}?>">
+        </div>
+        <hr>
+        <h4>Alamat Perusahaan</h4>
         <div class="form-group">
           <label>Jalan <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_street" value="<?php if($client != null){echo $client->client_street;}?>">
+          <input class="form-control" type="text" name="client_street" value="<?php if($client != null){echo $client->client_street;}?>">
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Kelurahan <small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_subdistrict" value="<?php if($client != null){echo $client->client_subdistrict;}?>">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Kecamatan <small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_district" value="<?php if($client != null){echo $client->client_district;}?>">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Kota/Kabupaten <small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_city" value="<?php if($client != null){echo $client->client_city;}?>">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Provinsi <small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_province" value="<?php if($client != null){echo $client->client_province;}?>">
+            </div>
+          </div>
+        </div>
+        <hr>
+        <h4>Kontak</h4>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Telepon 1<small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_phone_1" value="<?php if($client != null){echo $client->client_phone_1;}?>">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Telepon 2<small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_phone_2" value="<?php if($client != null){echo $client->client_phone_2;}?>">
+            </div>
+          </div>
         </div>
         <div class="form-group">
-          <label>Kecamatan <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_district" value="<?php if($client != null){echo $client->client_district;}?>">
+          <label>Email <small class="required-field">*</small></label>
+          <input class="form-control" type="text" name="client_email" value="<?php if($client != null){echo $client->client_email;}?>">
+        </div>
+        <hr>
+        <h4>Wajib Pajak</h4>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>NPWP<small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_npwp" value="<?php if($client != null){echo $client->client_npwp;}?>">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>NPWPD<small class="required-field">*</small></label>
+              <input class="form-control" type="text" name="client_npwpd" value="<?php if($client != null){echo $client->client_npwpd;}?>">
+            </div>
+          </div>
+        </div>
+        <h4>Pemilik</h4>
+        <div class="form-group">
+          <label>Nama Pemilik<small class="required-field">*</small></label>
+          <input class="form-control" type="text" name="client_owner_name" value="<?php if($client != null){echo $client->client_owner_name;}?>">
         </div>
         <div class="form-group">
-          <label>Kota <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_city" value="<?php if($client != null){echo $client->client_city;}?>">
+          <label>Alamat Pemilik<small class="required-field">*</small></label>
+          <textarea class="form-control" type="text" name="client_owner_address"><?php if($client != null){echo $client->client_owner_address;}?></textarea>
         </div>
+        <hr>
+        <h4>Data Lain</h4>
         <div class="form-group">
-          <label>Provinsi <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_province" value="<?php if($client != null){echo $client->client_province;}?>">
+          <label>Keterangan Tambahan<small class="required-field">*</small></label>
+          <textarea class="form-control" type="text" name="client_notes"><?php if($client != null){echo $client->client_notes;}?></textarea>
         </div>
         <div class="form-group">
           <label>Serial Number <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_serial_number" value="<?php if($client != null){echo $client->client_serial_number;}?>">
-        </div>
-        <div class="form-group">
-          <label>NPWP <small class="required-field">*</small></label>
-          <input class="form-control keyboard" type="text" name="client_npwp" value="<?php if($client != null){echo $client->client_npwp;}?>">
+          <input class="form-control" type="text" name="client_serial_number" value="<?php if($client != null){echo $client->client_serial_number;}?>">
         </div>
         <div class="form-group">
           <label>Virtual Keyboard?</label><br>
