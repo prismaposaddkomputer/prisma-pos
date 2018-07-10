6da0d0598bf89df96ffb89886e01ea617e764120
chrisnabayuaji <chrisnabayuaji@gmail.com>
2018-07-10 13:42:29 +0700

    Fix #17 bug

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -203,7 +203,7 @@ class MY_Hotel extends MX_Controller{
     $data['sidenav'] = $this->m_hot_config->get_list();
 
     $this->load->model('hot_client/m_hot_client');
-    $client = $this->m_res_client->get_all();
+    $client = $this->m_hot_client->get_all();
     $data['keyboard'] = $client->client_keyboard_status;
     $data['client'] = $client;
 
