f2c2469ee3b81bc374bfacea1f4601521633e851
chrisnabayuaji <chrisnabayuaji@gmail.com>
2018-07-10 11:14:50 +0700

    Update UI

--- application/modules/app_install/views/index.php
+++ application/modules/app_install/views/index.php
@@ -1,7 +1,8 @@
 <div class="col-md-8 col-md-offset-2">
   <div class="panel panel-default">
     <div class="panel-body text-center">
-      <img src="<?=base_url()?>img\prisma-pos.png" alt="" width="100">
+      <br>
+      <img src="<?=base_url()?>img\prisma-pos-big.png" alt="" width="220">
       <h3>Selamat datang di Prisma POS</h3>
       <p>Selamat telah menjadi bagian dari generasi transaksi modern.</p>
       <p>Prisma POS akan membantu anda dalam mengelola transaksi bisnis anda.</p>
