2be986ff43e6f5b57e1d948d16e99d6d945e8e7b
chrisnabayuaji <chrisnabayuaji@gmail.com>
2018-07-12 11:52:24 +0700

    Add virtual keyboard

--- application/modules/app_template/views/auth/header.php
+++ application/modules/app_template/views/auth/header.php
@@ -5,21 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Prisma-POS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Prisma-POS</title>
     <!-- Favicon -->
     <link rel="icon" href="<?=base_url()?>img/prisma-pos.png">
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="<?=base_url()?>vendor/bootstrap/css/bootstrap.min.css">
     <!-- Prisma POS Install CSS -->
-    <link rel="stylesheet" href="<?=base_url()?>dist/css/prismapos.auth.css">
+    <link rel="stylesheet" href="<?=base_url()?>dist/css/prismapos.install.css">
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="<?=base_url()?>vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Keyboard css -->
+    <link rel="stylesheet" href="<?=base_url()?>vendor/keyboard-master/dist/css/keyboard.min.css">
+    <!-- jquery ui css -->
+    <link rel="stylesheet" href="<?=base_url()?>vendor/jquery-ui/jquery-ui.min.css">
     <!-- jQuery CDN -->
     <script src="<?=base_url()?>vendor/jquery/jquery-3.3.1.min.js"></script>
+    <!-- JQuery UI -->
+    <script src="<?=base_url()?>vendor/jquery-ui/jquery-ui.min.js"></script>
     <!-- Bootstrap Js CDN -->
     <script src="<?=base_url()?>vendor/bootstrap/js/bootstrap.min.js"></script>
     <!-- JQuery Validation JS -->
     <script src="<?=base_url()?>vendor/jquery-validation/dist/jquery.validate.min.js" charset="utf-8"></script>
-    <!-- Custom Script -->
+    <!-- keyboard js -->
+    <script src="<?=base_url()?>vendor/keyboard-master/dist/js/jquery.keyboard.min.js"></script>
+    <script>
+      $(document).ready(function () {
+        $('.keyboard').keyboard();
+      })
+    </script>
+    <style media="screen">
+      div.ui-widget {
+        font-size: 1.5em;
+      }
+      button.ui-keyboard-button {
+        margin: 4px;
+      }
+    </style>
     <script type="text/javascript">
       setInterval(function () {
         $('.alert').fadeOut();
