d7311262fda14938af8fd5ab1bd7ba7ccedea6d0
chrisnabayuaji <chrisnabayuaji@gmail.com>
2018-07-18 11:04:39 +0700

    Change power button

--- application/modules/app_template/views/hotel/topbar.php
+++ application/modules/app_template/views/hotel/topbar.php
@@ -5,20 +5,29 @@
         <i class="fa fa-bars"></i>
       </button>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <div class="btn-group pull-right" role="group">
-          <button type="button" class="btn btn-danger navbar-btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-power-off"></i> Power
-            <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu">
-            <li><a href="<?=base_url()?>app_system/shutdown"><i class="fa fa-circle-o"></i> Shutdown</a></li>
-            <li><a href="<?=base_url()?>app_system/restart"><i class="fa fa-refresh"></i> Restart</a></li>
-            <li><a href="<?=base_url()?>app_auth/logout/logout_action"><i class="fa fa-sign-out"></i> Keluar</a></li>
-          </ul>
-        </div>
-      </ul>
-    </div>
+    <button type="button" class="btn btn-danger navbar-btn pull-right" data-toggle="modal" data-target=".modal-power"><i class="fa fa-power-off"></i></button>
   </div>
 </nav>
+<div class="modal fade modal-power" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+  <div class="modal-dialog modal-md" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-center">Prisma Point of Sales</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-4">
+            <a class="btn btn-danger btn-block" href="<?=base_url()?>app_system/shutdown"><i class="fa fa-circle-o"></i> Shutdown</a>
+          </div>
+          <div class="col-md-4">
+            <a class="btn btn-success btn-block" href="<?=base_url()?>app_system/restart"><i class="fa fa-refresh"></i> Restart</a>
+          </div>
+          <div class="col-md-4">
+            <a class="btn btn-default btn-block" href="<?=base_url()?>app_auth/logout/logout_action"><i class="fa fa-sign-out"></i> Logout</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
