e285dce4e73738af8900561be394d0ee3d3146c2
alfian1998 <alfian.muhammad1998@gmail.com>
2018-09-03 11:03:09 +0700

    update error

--- application/modules/hot_reservation/controllers/Hot_reservation.php
+++ application/modules/hot_reservation/controllers/Hot_reservation.php
@@ -372,7 +372,7 @@ class Hot_reservation extends MY_Hotel {
       $printer -> selectPrintMode(Escpos\Printer::MODE_FONT_A);
       $printer -> feed();
       $printer -> setJustification(Escpos\Printer::JUSTIFY_LEFT);
-      $printer -> text("Tn. Danu");
+      $printer -> text($billing->guest_name);
       $printer -> feed();
       $printer -> setJustification(Escpos\Printer::JUSTIFY_RIGHT);
       // $printer -> text("123287789");
