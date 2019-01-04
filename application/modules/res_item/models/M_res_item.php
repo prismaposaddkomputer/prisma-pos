cd5f3301a316a92b1097fa7df6ffd106ec5f8148
unknown <alfian.muhammad1998@gmail.com>
2019-01-04 17:14:59 +0700

    update upload image res_item

--- application/modules/res_item/models/M_res_item.php
+++ application/modules/res_item/models/M_res_item.php
@@ -54,11 +54,6 @@ class M_res_item extends CI_Model {
     return $this->db->order_by('item_id','desc')->get('res_item')->row();
   }
 
-  public function insert($data)
-  {
-    $this->db->insert('res_item',$data);
-  }
-
   public function update($id,$data)
   {
     $this->db->where('item_id',$id)->update('res_item',$data);
@@ -88,6 +83,122 @@ class M_res_item extends CI_Model {
 	{
 		$this->db
 			->insert('res_item_package', $data);
+	}
+
+	// public function insert($data)
+	// {
+	// 	$this->db->insert('res_item',$data);
+	// }
+
+	public function insert($data)
+	{
+		$data = $_POST;
+	    $client = $this->m_res_client->get_all();
+	    $tax = $this->m_res_tax->get_by_id($data['tax_id']);
+
+	    $last_id = $this->get_last();
+	    if ($last_id == null) {
+	      $item_id = 1;
+	    }else{
+	      $item_id = $last_id->item_id+1;
+	    };
+
+	    $data['created_by'] = $this->session->userdata('user_realname');
+	    if(!isset($data['is_active'])){
+	      $data['is_active'] = 0;
+	    }
+
+	    $item_price = price_to_num($data['item_price']);
+	    if ($client->client_is_taxed == 0) {
+	      $data['item_price_before_tax'] = $item_price;
+	      $data['item_tax'] = ($tax->tax_ratio/100)*$item_price;
+	      $data['item_price_after_tax'] = $data['item_price_before_tax']+$data['item_tax'];
+	    }else{
+	      $data['item_price_after_tax'] = $item_price;
+	      $data['item_tax'] = ($tax->tax_ratio/(100+$tax->tax_ratio))*$data['item_price_after_tax'];
+	      $data['item_price_before_tax'] = $data['item_price_after_tax']-$data['item_tax'];
+	    }
+	    // $data['tax_name'] = $tax->tax_name;
+	    // $data['tax_ratio'] = $tax->tax_ratio;
+
+	    $item_logo = $this->compress_image('item_logo');
+
+	    if ($data['item_logo_input'] !='') {
+	      if ($item_logo !='') {
+	        $data['item_logo'] = $item_logo;
+	      }else{
+	        $data['item_logo'] = $data['item_logo_input'];
+	      }
+	    }elseif ($item_logo !='') {
+	      $data['item_logo'] = $item_logo;
+	    }
+
+	    unset($data['item_price'], $data['item_logo_input']);
+
+	    // clear package
+	    $this->clear_package($item_id);
+	    // insert package
+	    if(isset($data['item_detail_id'])){
+	      foreach ($data['item_detail_id'] as $key => $val) {
+	        $data_package = null;
+	        $data_package = array(
+	          "item_id" => $item_id,
+	          "item_detail_id" => $val,
+	          "item_detail_price" => $data['item_detail_price'][$key]
+	        );
+	        $this->insert_package($data_package);
+	      }
+	    }
+
+	    unset($data['item_detail_id'],$data['item_detail_price']);
+
+		$this->db->insert('res_item',$data);
+	}
+
+	public function compress_image($src_file_name = null) {
+	    // data tanggal
+	    $this->load->library('upload');
+	    $this->load->library('image_lib');
+
+	    $config['upload_path'] = 'img/res_item/'; //path folder
+	    $config['allowed_types'] = 'jpg|png|jpeg'; //type yang dapat diakses bisa anda sesuaikan
+
+	    $this->upload->initialize($config);
+	    if(!empty($_FILES[$src_file_name]['name'])){
+	        
+	        if ($this->upload->do_upload($src_file_name)){
+	            $gbr = array('upload_data' => $this->upload->data()); 
+	            // cek resolusi gambar
+	            $nama_gambar = 'img/res_item/'.$gbr['upload_data']['file_name'];
+	            $data = getimagesize($nama_gambar);
+	            $width_normal = $data[0];
+	            $height_normal = $data[1];
+	            // pembagian
+	            // $bagi_width = $width / 4;
+	            // $bagi_height = $height / 4;
+	            //
+	            $width_akhir = 100;
+	            // $height_akhir = ($width_akhir / $width_normal) * $height_normal;
+	            $height_akhir = 100;
+	            //Compress Image
+	            $config['image_library']='gd2';
+	            $config['source_image'] = $gbr['upload_data']['full_path'];
+	            $config['create_thumb']= FALSE;
+	            $config['maintain_ratio']= FALSE;
+	            $config['quality']= '100%';
+	            $config['width']= $width_akhir;
+	            $config['height']= $height_akhir;
+	            $config['new_image']= 'img/res_item/'.$gbr['upload_data']['file_name'];
+	            $this->image_lib->initialize($config);
+	            // $this->load->library('image_lib', $config);
+	            $this->image_lib->resize();
+	            $this->image_lib->clear();
+
+	            $item_logo=$gbr['upload_data']['file_name'];
+	        }
+	        //
+	        return $item_logo;  
+	    }           
 	}
 
 }
