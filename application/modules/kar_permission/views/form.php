267e1ce342f3e5644a88d98d313950adcffb288f
unknown <alfian.muhammad1998@gmail.com>
2019-03-20 14:16:07 +0700

    update 20-03-2019 14:15

--- application/modules/kar_permission/views/form.php
+++ application/modules/kar_permission/views/form.php
@@ -1,5 +1,8 @@
 <div class="content-header">
-  <h4><i class="fa fa-<?=$access->module_icon?>"></i> <?=$title?></h4>
+  <h4>
+    <a href="<?=base_url('kar_permission')?>" class="btn btn-success"><i class="fa fa-arrow-left"></i></a> 
+    <i class="fa fa-<?=$access->module_icon?>"></i> <?=$title?>
+  </h4>
 </div>
 <div class="content-body">
   <div class="row">
